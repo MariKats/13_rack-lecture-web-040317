fae11e45042b9313870d67b7a50116804312edd7
ipc103 <iancandysemail@gmail.com>
2017-04-24 16:34:14 -0400

    post lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,42 @@ class ApplicationController
 
   def call(env)
     request = Rack::Request.new(env)
-    response = Rack::Response.new
     if request.path == '/'
-      response.write '<h1>Hello World</h1><p>Some more info about our site...</p>'
+      render "index.html.erb"
+    elsif request.params['id'] && request.path == '/books'
+      @book = Book.find(request.params["id"])
+      render("books/show.html.erb")
     elsif request.path == '/books'
-      response.write 'Books Page'
+      @books = Book.all
+      render "books/index.html.erb"
     else
+      response = Rack::Response.new
       response.status = '404'
       response.write 'Not found'
+      response.finish
     end
+  end
+
+  private
+
+  def render(file_path)
+    response = Rack::Response.new
+    template = File.read("./app/views/#{file_path}")
+    body = ERB.new(template).result(binding)
+    response.write(body)
     response.finish
   end
 end
+
+# class BooksController
+#
+#   def index
+#     response = Rack::Response.new
+#     @books = Book.all
+#     template = File.read('./app/views/books/index.html.erb')
+#     body = ERB.new(template).result(binding)
+#     response.write(body)
+#     response.finish
+#   end
+#
+# end
